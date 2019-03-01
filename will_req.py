6d5535bf57d8e4d3be866da5b219be07a45d43a6
Will World <alt2.richardyi25@gmail.com>
2019-03-01 11:56:20 -0500

    test

--- will_req.py
+++ will_req.py
@@ -8,17 +8,15 @@ titles = []
 contents = []
 
 for en in e:
-    link = en.links[0].href
-    g = get(link)
-    p = pyq(g.text)
-    title = p('h2')
-    titles.append(title.text())
-    content = p('#left-div>.single-post-wrap>p')
-    contents.append(content.text())
-
+	link = en.links[0].href
+	g = get(link)
+	p = pyq(g.text)
+	title = p('h2')
+	titles.append(title.text())
+	content = p('#left-div>.single-post-wrap>p')
+	contents.append(content.text())
 
 length = len(titles)
 with open('index.html', 'w') as f:
-    for i in range(length):
-        f.write('<h2>%s</h2>' % titles[i])
-        f.write('<p style="background-color:hsl(' + str(float(i / length * 360)) + ', 90%, 90%);font-size:20px;padding:1%;">' + contents[i] + '</p><hr/>')
+	for i in range(length):
+		f.write('<div style="border-radius:1vw;background-color:hsl(' + str(float(i / length * 360)) + ', 90%, 90%);font-size:20px;padding:2vh;margin:2vh;"><h2>' + titles[i] + '</h2>' + contents[i] + '</div>')
